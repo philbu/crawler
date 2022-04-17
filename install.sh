17730b2f52a7a391b67dd36e842231236f5d68a1
phil <phil.buschmann@tum.de>
2022-04-17 23:40:41 +0200

    Fix virtualenv

--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ echo "Create directory at \"$app_dir\":"
 mkdir "$app_dir" && cd "$app_dir"
 echo "Clone git repository:"
 git clone https://github.com/philbu/crawler.git "git"
-python3 -m pip install --user virtualenv && python3 -m venv env && source env/bin/activate && pip install -r git/requirements.txt
+echo "Create virtual environment for python (virtualenv)"
+python3 -m virtualenv env && source env/bin/activate && pip install -r git/requirements.txt
 
 echo "Create cronjob script:"
-echo "#!/bin/bash" > cronjob.sh
-echo "$app_dir/env/bin/python3 -m $app_dir/git/crawler" >> cronjob.sh
+echo "#!/bin/bash" > "$app_dir/cronjob.sh"
+echo "$app_dir/env/bin/python3 -m $app_dir/git/crawler" >> "$app_dir/cronjob.sh"
 
 echo "Next steps:"
 echo "1. Add missing information to git/crawler/config.ini"
